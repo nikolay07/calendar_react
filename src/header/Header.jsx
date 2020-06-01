8522a73dedeac37349c19ebed51ec49d0779fb77
nikolay07 <nikolay0701@ukr.net>
2020-06-01 12:43:50 +0300

    1

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-
 import { getDisplayedMonth } from "../common/timeUtils";
+
 // import PropTypes from "prop-types";
 
-const Header = ({
-  week,
-  onToday,
-  nextWeek,
-  prewWeek,
-  onCreateEvent,
-}) => {
+const Header = ({ week, onToday, nextWeek, prewWeek, onCreateEvent }) => {
   const currentMonth = getDisplayedMonth(week);
   return (
     <header className="header">
@@ -22,30 +16,18 @@ const Header = ({
         <i className="fas fa-plus" />
         Create
       </button>
-      <button
-        className="header__button_today"
-        onClick={onToday}
-        type="button"
-      >
+      <button className="header__button_today" onClick={onToday} type="button">
         Today
       </button>
       <div className="header__week-toggle">
-        <span
-          onClick={prewWeek}
-          className="header__week-toggle_chevron-left"
-        >
+        <span onClick={prewWeek} className="header__week-toggle_chevron-left">
           <i className="fas fa-chevron-left chevron" />
         </span>
-        <span
-          onClick={nextWeek}
-          className="header__week-toggle_chevron-right"
-        >
+        <span onClick={nextWeek} className="header__week-toggle_chevron-right">
           <i className="fas fa-chevron-right chevron" />
         </span>
       </div>
-      <span className="header__current-month-year">
-        {currentMonth}
-      </span>
+      <span className="header__current-month-year">{currentMonth}</span>
     </header>
   );
 };
